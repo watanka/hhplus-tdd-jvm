f7532cb6124cfbb888f02afcae0b06c90e876d35
eunsung <edplab2019@gmail.com>
2024-03-18 21:12:40 +0900

    Initial commit

---
+++ src/test/java/io/hhplus/tdd/database/PointHistoryTableTest.java
@@ -0,0 +1,16 @@
+package io.hhplus.tdd.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointHistoryTableTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void selectAllByUserId() {
+    }
+}
