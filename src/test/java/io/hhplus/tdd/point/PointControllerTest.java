a32ad2ab75bf8090b7a7ab65ee2cb00126dcff3d
eunsung <eunsung.shin@gmail.com>
2024-03-20 00:43:59 +0900

    전체 서비스(e2e) 테스트 코드 추가:포인트 조회

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,13 +1,44 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.service.PointService;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 class PointControllerTest {
 
+    @Autowired
+    private PointController pointController;
+
+    @MockBean
+    private PointService pointService;
+
     @Test
-    void point() {
+    void 포인트_조회() {
+        //given: 3000 포인트를 가진 유저 등록
+        long userId = 1L;
+        long amount = 3000L;
+
+
+        UserPoint userPoint = new UserPoint(userId, amount, System.currentTimeMillis());
+        // 이부분이 맞는 코드인지 모르겠다. 이렇게 조건을 셋팅하는 게 맞는지?
+        when(pointService.getUserPointById(userId)).thenReturn(userPoint);
+
+        //when: 유지 1의 포인트 조회
+        UserPoint result = pointController.point(1);
+
+        //then: 유저 1의 포인트 리턴
+        assertEquals(userId, result.id());
+        assertEquals(amount, result.point());
     }
 
     @Test
